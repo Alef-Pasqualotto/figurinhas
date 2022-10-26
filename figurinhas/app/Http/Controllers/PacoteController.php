6352c9371d4c99a4fdd0bb1578802db286449c02
Alef-Pasqualotto <pasqualotto.alef@gmail.com>
2022-10-26 09:17:16 -0300

    funcoes pacotes

--- figurinhas/app/Http/Controllers/PacoteController.php
+++ figurinhas/app/Http/Controllers/PacoteController.php
@@ -9,14 +9,27 @@ class PacoteController extends Controller
 {    
     function criarPacote(Request $request){
         
-        unset($data['_token']);
+        //unset($data['_token']);
         
-        $id = array_shift($data);    
+        //$id = array_shift($data);
+        $id = 1;
         DB::table('pacotes')->insert([
             'usuario_id' => $id,
             'data_compra' => NOW()
         ]);
 
+        $figurinhas_aleatorias = DB::table('figurinhas')->select('id')->orderBy('rand()');
+
+
+        
         return redirect('/cursos');
+    }
+
+    function insereFigurinhaPacote($pacote_id, $figurinha_id){
+        DB::table('figuirinhas_pacotes')->insert([
+            'figurinha_id' => $figurinha_id,
+            'pacote_id' => $pacote_id,
+            'colada' => 0
+        ]);
     }
 }
