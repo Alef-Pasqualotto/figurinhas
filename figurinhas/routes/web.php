3b5907249cfa0040ca2b70e3af15955f5dc1b7b4
Cesar Torbes <cesar.110205@bento.local>
2022-10-26 08:36:48 -0300

    rota criada

--- figurinhas/routes/web.php
+++ figurinhas/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FigurinhaController;
+use App\Http\Controllers\PacoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get('/figurinhas', [FigurinhaController::class, 'index']);
+Route::get('/pacotes', [PacoteController::class, 'criarPacote']);
