e7dd03dcf51872fcd17e0005b69e8f5806926483
Mateus Haupt <104838329+mateushaupt@users.noreply.github.com>
2022-11-30 09:08:06 -0300

    livrin reação

--- figurinhas/resources/views/teste/index.blade.php
+++ figurinhas/resources/views/teste/index.blade.php
@@ -1,18 +1,13 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel React</title>
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
-<div id="example">
-    <p>oi</p>
-</div>
-
-
-
-<script src="js/app.js"></script>
+    <div id="hello-react"></div>
 </body>
 </html>
